69d1a124c01a18204ccbe2329908bed3ead57c0a
Will Vetter <vetter.william@gmail.com>
2020-09-08 05:01:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   my_name = nil
 
   name_hash.each do |name, item|
-    if item < smallest
+    if item < smallest or smallest == nil
       my_name = name
     end
   end
