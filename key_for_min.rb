6d8a1bde84a754d98c19698f600f27ea05d51ee8
Will Vetter <vetter.william@gmail.com>
2020-09-08 04:57:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = 1000
+  smallest = name_hash[0]
 
-  name_hash.each do |item|
+  name_hash.each do |name, item|
     if item < smallest
       smallest = item
     end
